5168d7f1545059e680752d31aa524974d7d86d34
Catelyn-Close <catie.did75@gmail.com>
2024-07-31 14:09:40 -0700

    Minimum wage savings calc

---
+++ Savings calculator.py
@@ -0,0 +1,13 @@
+# create code for savings calculator. How many months should save to reach target goal
+# assume salary is minimum wage
+
+
+print("minimum Wage Savings Calculator")
+print("The program will tell you many months you will need to work")
+s = float(input("enter wanted savings:"))
+
+m = 1257
+a = s/m
+
+print(round(a))
+print("months")
