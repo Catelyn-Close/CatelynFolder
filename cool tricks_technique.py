3ec3241c1f70a72f68b3ae16ea386783cac16665
Catelyn-Close <catie.did75@gmail.com>
2024-08-05 12:53:47 -0700

    Update cool tricks_technique.py

--- cool tricks_technique.py
+++ cool tricks_technique.py
@@ -9,3 +9,24 @@ if age >= 16 and license:
     print("You are old enough to drive.")
 else:
     print("you are not able to drive.")
+
+_______________________________________________________
+#made madlib, formatted w/ list. hated it
+
+noun1 = input("Choose a noun: ") 
+
+pnoun1 = input("Choose a plural noun: ")
+
+noun2 = input("Choose a noun: ")
+
+place = input("Choose a place: ")
+
+adjective = input("Choose an adjective Describing word: ")
+
+madlib = "I love my {0}. They are very cool. I also like my  {1}. I take both of these everywhere I go, but i also like {2}! I take them many places like {3} for example! People look at me like im {4} but thats okay!"
+
+Wordbank = [noun1, pnoun1, noun2, place, adjective]
+
+
+formatted_madlib = madlib.format(*Wordbank)
+print(formatted_madlib)
