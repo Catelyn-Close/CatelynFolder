fd533a9e2f0333a714583b63166fdf46a17cb6ca
Catelyn-Close <catie.did75@gmail.com>
2024-08-05 13:53:23 -0700

    made a second calc

--- Savings calculator.py
+++ Savings calculator.py
@@ -11,3 +11,21 @@ a = s/m
 
 print(round(a))
 print("months")
+
+
+____________________________________________
+#made another, bit different
+
+earningsgoal = int(input("How much money do you want to save? "))
+
+months = round(earningsgoal/12, 2)
+
+weeks = round(months/52, 2)
+
+days = round(earningsgoal/365, 2)
+
+print("To save up " + str(earningsgoal) + " per year, you will need to save " + str(months) + " dollars per month")
+print("To save up " + str(earningsgoal) + " per year, you will need to save " + str(weeks) + " dollars per week")
+print("To save up " + str(earningsgoal) + " per year, you will need to save " + str(days) + " dollars per day")
+
+
