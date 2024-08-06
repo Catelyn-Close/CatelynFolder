aa85bee317797d09962c2909987a83c42580edf4
Catelyn-Close <catie.did75@gmail.com>
2024-08-06 12:31:35 -0700

    adding team roster project

--- python coding projects.py
+++ python coding projects.py
@@ -1,3 +1,4 @@
+#small counter thing
 student = 10
 
 classroom = student*30
@@ -13,3 +14,24 @@ goal = 10000
 pagesperstudent = goal/360
 
 print(pagesperstudent)
+__________________________________________________________
+#team roster printout
+string1 = "Welcome to the team {}! Your team number is {}"
+
+# Define a list of tuples with player names and their numbers
+players = [
+    ("Jane", "1"),
+    ("Doe", "2"),
+    ("Smith", "3"),
+    ("John", "4"),
+    ("David", "5"),
+    ("Tennant", "6"),
+    ("River", "7"),
+    ("Amy", "8"),
+    ("Rory", "9"),
+    ("Bill", "10")
+]
+
+# Print each player's name and number using the format method
+for player in players:
+    print(string1.format(player[0], player[1]))
